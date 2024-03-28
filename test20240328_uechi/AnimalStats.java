527d6698e44e1da342e87b9ad88c62975ac6c6e6
tkamasa <tkms0831@gmail.com>
2024-03-28 15:07:05 +0900

    javatest

---
+++ test20240328_uechi/AnimalStats.java
@@ -0,0 +1,37 @@
+package JavaTest2.test20240328_uechi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnimalStats {
+    public static void printAnimalStatistics() {
+        // Animalオブジェクトのリストを作成
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Animal("ライオン", 120.0, 190.0));
+        animals.add(new Animal("ゾウ", 300.0, 6000.0));
+        animals.add(new Animal("チーター", 90.0, 72.0));
+        animals.add(new Animal("キリン", 500.0, 1200.0));
+        animals.add(new Animal("カバ", 150.0, 1500.0));
+        animals.add(new Animal("トラ", 110.0, 220.0));
+
+        // 最大値と最小値を求める
+        double maxHeight = Double.MIN_VALUE;
+        double minHeight = Double.MAX_VALUE;
+        double maxWeight = Double.MIN_VALUE;
+        double minWeight = Double.MAX_VALUE;
+
+        for (Animal animal : animals) {
+            if (animal.getHeight() > maxHeight) maxHeight = animal.getHeight();
+            if (animal.getHeight() < minHeight) minHeight = animal.getHeight();
+            if (animal.getWeight() > maxWeight) maxWeight = animal.getWeight();
+            if (animal.getWeight() < minWeight) minWeight = animal.getWeight();
+        }
+
+        // 結果の出力
+        System.out.println("動物６頭の体重・身長を計測しました。身長と体重の最大値と最小値を出力しています。");
+        System.out.println("最大身長: " + maxHeight + " cm");
+        System.out.println("最小身長: " + minHeight + " cm");
+        System.out.println("最大体重: " + maxWeight + " kg");
+        System.out.println("最小体重: " + minWeight + " kg");
+    }
+}
---
+++ test20240328_uechi/PasswordInput.java
@@ -0,0 +1,49 @@
+package JavaTest2.test20240328_uechi;
+
+import java.util.Scanner;
+
+public class PasswordInput {
+    public static void getInputAndCheckPassword() {
+        // ルールの説明
+        System.out.println("パスワードは8文字以上で、文字と数字のみを含む必要があります。数字は3つ以上含む必要があります。");
+
+        // パスワードの入力
+        System.out.println("パスワードを入力してください:");
+        Scanner scanner = new Scanner(System.in);
+        String password = scanner.nextLine();
+
+        // パスワードのチェック
+        if (isValidPassword(password)) {
+            System.out.println("パスワードは有効です。");
+        } else {
+            System.out.println("パスワードは無効です。");
+        }
+    }
+
+    // パスワードが有効かどうかをチェックするメソッド
+    public static boolean isValidPassword(String password) {
+        // パスワードの長さが8文字以上であることを確認
+        if (password.length() < 8) {
+            return false;
+        }
+
+        // 数字の数をカウント
+        int digitCount = 0;
+        for (char c : password.toCharArray()) {
+            if (Character.isDigit(c)) {
+                digitCount++;
+            } else if (!Character.isLetter(c)) {
+                // 文字か数字以外が含まれている場合は無効
+                return false;
+            }
+        }
+
+        // 数字の数が3つ以上であることを確認
+        if (digitCount < 3) {
+            return false;
+        }
+
+        // 全ての条件を満たしていれば有効なパスワード
+        return true;
+    }
+}
